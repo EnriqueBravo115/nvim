734d73faf7be2e53803eae5a67e7946944251d84
EnriqueBravo115 <enriquebravo115@gmail.com>
2023-05-27 11:27:24 -0600

    Brutal changes

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,4 +1,5 @@
 local dap = require('dap')
+
 dap.configurations.java = {
     {
         name = "Debug (Attach) - Remote",
@@ -12,29 +13,6 @@ dap.configurations.java = {
         type = "java",
         request = "launch",
         program = "",
-    },
-    {
-        name = "Debug Non-Project class",
-        type = "java",
-        request = "launch",
-        program = "${file}",
-    },
-}
-
-dap.adapters.coreclr = {
-    type = 'executable',
-    command = '/home/nullboy/Applications/netcoredbg/netcoredbg',
-    args = { '--interpreter=vscode' }
-}
-
-dap.configurations.cs = {
-    {
-        type = "coreclr",
-        name = "launch - netcoredbg",
-        request = "launch",
-        program = function()
-            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-        end,
     },
 }
 
@@ -56,26 +34,6 @@ require("dapui").setup({
     -- Expand lines larger than the window
     -- Requires >= 0.7
     expand_lines = vim.fn.has("nvim-0.7"),
-    --[[sidebar = {
-    -- You can change the order of elements in the sidebar
-    elements = {
-      -- Provide as ID strings or tables with "id" and "size" keys
-      {
-        id = "scopes",
-        size = 0.25, -- Can be float or integer > 1
-      },
-      { id = "breakpoints", size = 0.25 },
-      { id = "stacks", size = 0.25 },
-      { id = "watches", size = 00.25 },
-    },
-    size = 40,
-    position = "left", -- Can be "left", "right", "top", "bottom"
-  },
-  tray = {
-    elements = { "repl", "console" },
-    size = 10,
-    position = "bottom", -- Can be "left", "right", "top", "bottom"
-  },--]]
     layouts = {
         {
             elements = {
@@ -118,7 +76,3 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
     dapui.close()
 end
-
-vim.api.nvim_set_keymap("n", "<leader>hh",
-    ":lua require'dap'.run({ type = 'java', request = 'attach', name = 'Debug (Attach) - Remote', hostName = '127.0.0.1', port = 5005 })<CR>",
-    { noremap = true })
